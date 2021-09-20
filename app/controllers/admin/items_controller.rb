be6703023e618d7b331e79d1a6bf2e6819f9b69e
mizuki-konnno <m.m.mizuki.0215@gmail.com>
2021-09-20 01:11:45 +0000

    [Add]商品編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -19,8 +19,14 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
+  end
  private
 
   def item_params
